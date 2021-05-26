fff467176e0d3cdd52bb26e03e95779f77543e27
tutu-tonton <50165379+tutu-tonton@users.noreply.github.com>
2021-05-26 17:34:11 +0900

    クエリパラメータ扱い方 まずクエリパラメータ自体を取得する。 次に扱いやすくする。色々メソッド使えるようになる

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -5,6 +5,8 @@ export const Page2 = () => {
     <div>
       <h1>Page2</h1>
       <Link to="/page2/100">URL Parameter Page</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query Params Page</Link>
     </div>
   );
 };
